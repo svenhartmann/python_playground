5e60fe868fb00ccf7f30859422c30e9d55ac5f66
Sven Hartmann <info@3mal12.de>
2018-03-06 20:00:53 +0100

    Make pylint happy

--- src/test/test_simplecalc.py
+++ src/test/test_simplecalc.py
@@ -4,6 +4,7 @@ Unit test
 import unittest
 import simplecalc
 
+
 class TestSimplecalc(unittest.TestCase):
     """TestCase"""
     def test_add_int(self):
@@ -16,6 +17,7 @@ class TestSimplecalc(unittest.TestCase):
         """Sub test"""
         result = simplecalc.sub(1, 1)
         self.assertEqual(result, 0)
+
 
 if __name__ == '__main__':
     unittest.main()
--- src/test/test_simplecalcmocked.py
+++ src/test/test_simplecalcmocked.py
@@ -2,14 +2,14 @@
 Test mocking in Python unit tests..
 """
 import unittest
-import simplecalc
 from unittest.mock import patch
+
 
 class TestMockedSimplecalc(unittest.TestCase):
     """TestCase"""
 
     @patch('simplecalc.add', return_value=11)
-    def test_add_int(self, add ):
+    def test_add_int(self, add):
         """Add test mocked"""
         result = add(8, 9)
         self.assertEqual(result, 11)
@@ -19,8 +19,9 @@ class TestMockedSimplecalc(unittest.TestCase):
     @patch('simplecalc.sub', return_value=344)
     def test_sub_int(self, sub):
         """Sub test mocked"""
-        result = simplecalc.sub(1, 34)
+        result = sub(1, 34)
         self.assertEqual(result, 344)
+
 
 if __name__ == '__main__':
     unittest.main()
